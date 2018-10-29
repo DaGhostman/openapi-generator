2555f0d30cdbf51435fed1dbe4ba37af3a1918a6
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-10-29 09:39:39 +0200

    Improve parser interface

--- src/Interfaces/ParserInterface.php
+++ src/Interfaces/ParserInterface.php
@@ -1,7 +1,9 @@
-<?php declare(stricty_types=1);
+<?php declare(strict_types=1);
 namespace OpenAPI\Interfaces;
+
+use OpenAPI\Spec\Entities\Document;
 
 interface ParserInterface
 {
-    public function parse(string $file): array;
+    public function parse(string $file): Document;
 }
